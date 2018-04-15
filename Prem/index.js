85b94150dc7e051f5a08cfa99a217c5e8865b1a8
Rips <realrsquare@gmail.com>
2018-04-15 23:00:09 +0530

    More Feature

--- Prem/index.js
+++ Prem/index.js
@@ -22,14 +22,20 @@ con.connect(function (err) {
 app.post("/users", function (req, res) {
     var sql = "INSERT INTO users VALUE ('" + req.body.name + "', '" + req.body.team + "', '" + req.body.points + "');";
     con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Record inserted");
+        if (err) {
+            if (err.code != "ER_DUP_ENTRY") {
+                console.log("Record inserted");
+            } else {
+                var sql1 = "UPDATE users SET team='" + req.body.team + "', points='" + req.body.points + "' where name = '" + req.body.name + "';";
+                con.query(sql1, function (errR, result1) {
+                    if (errR) throw errR;
+                    result = "Data has been updated";
+                });
+            }
+        }
         res.send(result);
-
     });
-
 });
-
 
 app.post("/mult", function (req, res) {
     var sql1 = req.body.query1;
@@ -41,36 +47,41 @@ app.post("/mult", function (req, res) {
 });
 
 app.post("/ten", function (req, res) {
-    var sql = req.body.query;
-    con.query(sql, function (err, result) {
+    var sql1 = req.body.query1;
+    var sql2 = req.body.query2;
+    con.query((sql1 + sql2), function (err, result) {
         if (err) throw err;
         res.send(result);
     });
 });
 app.post("/20", function (req, res) {
-    var sql = req.body.query;
-    con.query(sql, function (err, result) {
+    var sql1 = req.body.query1;
+    var sql2 = req.body.query2;
+    con.query((sql1 + sql2), function (err, result) {
         if (err) throw err;
         res.send(result);
     });
 });
 app.post("/30", function (req, res) {
-    var sql = req.body.query;
-    con.query(sql, function (err, result) {
+    var sql1 = req.body.query1;
+    var sql2 = req.body.query2;
+    con.query((sql1 + sql2), function (err, result) {
         if (err) throw err;
         res.send(result);
     });
 });
 app.post("/40", function (req, res) {
-    var sql = req.body.query;
-    con.query(sql, function (err, result) {
+    var sql1 = req.body.query1;
+    var sql2 = req.body.query2;
+    con.query((sql1 + sql2), function (err, result) {
         if (err) throw err;
         res.send(result);
     });
 });
 app.post("/50", function (req, res) {
-    var sql = req.body.query;
-    con.query(sql, function (err, result) {
+    var sql1 = req.body.query1;
+    var sql2 = req.body.query2;
+    con.query((sql1 + sql2), function (err, result) {
         if (err) throw err;
         res.send(result);
     });
@@ -82,7 +93,16 @@ app.post("/delete_db", function (req, res) {
     console.log(sql);
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Record delete" + result);
+        res.send(result);
+    });
+});
+
+app.post("/select_data", function (req, res) {
+    // console.log(req.body.query + " req.body");
+    var sql = req.body.query1;
+    console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
         res.send(result);
     });
 });
